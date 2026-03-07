6afc8ece7cbd4a15da89ab0b2e7211bc37f523be
srebello25 <srebello25@yahoo.com>
2026-03-07 18:44:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+﻿function calculate(){
+
+    const a = parseFloat(document.getElementById("a").value);
+    const b = parseFloat(document.getElementById("b").value);
+    const op = document.getElementById("op").value;
+
+    let result;
+
+    if(op === "add"){
+        result = a + b;
+    }
+    else if(op === "sub"){
+        result = a - b;
+    }
+    else if(op === "mul"){
+        result = a * b;
+    }
+    else if(op === "div"){
+        result = a / b;
+    }
+
+    document.getElementById("result").innerText =
+        "Result: " + result;
+
+}
