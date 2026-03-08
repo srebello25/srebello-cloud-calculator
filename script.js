04d316c96c856752e574d7412a21287fdf359b3a
Sibelius Rebello <srebello25@yahoo.com>
2026-03-08 23:48:45 +0000

    connect frontend to AWS API

---
+++ script.js
@@ -0,0 +1,34 @@
+﻿async function calculate() {
+    const a = parseFloat(document.getElementById("a").value);
+    const b = parseFloat(document.getElementById("b").value);
+    const op = document.getElementById("op").value;
+
+    if (isNaN(a) || isNaN(b)) {
+        document.getElementById("result").innerText = "Enter both numbers";
+        return;
+    }
+
+    try {
+        const response = await fetch("http://13.40.188.144:5000/api/calc", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                a: a,
+                b: b,
+                op: op
+            })
+        });
+
+        const data = await response.json();
+
+        if (data.result !== undefined) {
+            document.getElementById("result").innerText = "Result: " + data.result;
+        } else {
+            document.getElementById("result").innerText = "Error";
+        }
+    } catch (error) {
+        document.getElementById("result").innerText = "Cannot connect to API";
+    }
+}
